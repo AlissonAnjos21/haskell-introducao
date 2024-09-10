abe03eddb97b91a911a05678181efd86d2d7420d
AlissonAnjos21 <93294707+AlissonAnjos21@users.noreply.github.com>
2024-09-10 16:16:32 -0300

    SomaPA e PosicaoPlanoCartesiano

--- lista01/exercicios.hs
+++ lista01/exercicios.hs
@@ -12,3 +12,22 @@ tipoTriangulo x y z
   | ((x > 90) || (y > 90) || (z > 90)) && (((x + y + z) > 0) && (x + y + z) <= 180 ) = "Obtusangulo"
   | ((x < 90) || (y < 90) || (z < 90)) && (((x + y + z) > 0) && (x + y + z) <= 180 ) = "Acutangulo"
   | otherwise = "Nao eh triangulo"
+
+calcularTermon :: Float -> Float -> Float -> Float 
+calcularTermon termo1 n passo = termo1 + ((n-1)*passo) 
+
+somaPA :: Float -> Float -> Float -> Float
+somaPA termo1 passo n = ((termo1 + (calcularTermon termo1 n passo)) * n)/2
+-- somaPA termo1 termon n = ((termo1 + termon)*n)/2
+
+posicaoPlanoCartesiano :: Float -> Float -> String
+posicaoPlanoCartesiano x y 
+  | x == 0 && y == 0 = "Origem"
+  | x > 0 && y == 0 = "Eixo X positivo"
+  | x < 0 && y == 0 = "Eixo X negativo"
+  | x == 0 && y > 0 = "Eixo Y positivo"
+  | x == 0 && y < 0 = "Eixo Y negativo"
+  | x > 0 && y > 0 = "Primeiro quadrante"
+  | x < 0 && y > 0 = "Segundo quadrante"
+  | x < 0 && y < 0 = "Terceiro quadrante"
+  | x > 0 && y < 0 = "Quarto quadrante"
