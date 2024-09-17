6e8244ea588d6d14bfa7dd5cbb49306c0eb530b8
AlissonAnjos21 <93294707+AlissonAnjos21@users.noreply.github.com>
2024-09-17 02:43:44 -0300

    Exercicios Haskell

--- lista02/exercicios.hs
+++ lista02/exercicios.hs
@@ -45,3 +45,92 @@ somarPorSucessor :: Int -> Int -> Int
 somarPorSucessor x y
   | y == 0 = x
   | otherwise = 1 + (somarPorSucessor x (y-1))
+
+multiplicacaoPorSoma :: Int -> Int -> Int
+multiplicacaoPorSoma x y
+  | y == 0 = 0
+  | otherwise = x + (multiplicacaoPorSoma x (y - 1))
+
+quantidadeDigitos :: Int -> Int
+quantidadeDigitos x
+  | (x `div` 10) == 0 = 1
+  | otherwise = 1 + (quantidadeDigitos (x `div` 10))
+
+concatenarNvezes :: String -> Int -> String
+concatenarNvezes palavra n 
+  | n == 0 = palavra
+  | otherwise = palavra ++ (concatenarNvezes palavra (n-1))
+
+decimalParaBinario :: Int -> String
+decimalParaBinario n 
+  | (n `div` 2) == 0 || (n `div` 2) == 1 = show(n `div` 2) ++ show(n `mod` 2)
+  | otherwise = (decimalParaBinario (n `div` 2)) ++ show(n `mod` 2)
+
+converteParaListaDeInt :: Int -> [Int]
+converteParaListaDeInt x = map(\x -> read[x] :: Int) (show x)
+
+aparicoesKnoNaturalN :: Int -> Int -> Int
+aparicoesKnoNaturalN n k = length( (filter(==k)) (converteParaListaDeInt n))
+
+fibonacci :: Int -> Int
+fibonacci n
+  | n == 0 = 0
+  | n == 1 = 1
+  | otherwise = (fibonacci (n-1)) + (fibonacci (n-2))
+
+fibonacciTail :: Int -> Int
+fibonacciTail 0 = 0
+fibonacciTail 1 = 1
+fibonacciTail n = (fibonacciTail (n-1)) + (fibonacciTail (n-2))
+
+mdc :: Int -> Int -> Int
+mdc a b
+  | b == 0 = a
+  | otherwise = (mdc b (a `mod` b))
+
+tabuadaN :: Int -> Int -> String
+tabuadaN n m
+  | m == 0 = (show 0)
+  | otherwise = (show (n * m)) ++ " " ++ (tabuadaN n (m-1))
+
+calcularNumeroEulerComNtermos :: Int -> Float
+calcularNumeroEulerComNtermos n
+  | n == 0 = 1
+  | otherwise = (1 / fromIntegral (fatorial n)) + (calcularNumeroEulerComNtermos (n-1))
+
+tabelaValoresNumeroEuler :: Int -> String
+tabelaValoresNumeroEuler t
+  | t == 1 = show (calcularNumeroEulerComNtermos 1)
+  | t > 1 = show (calcularNumeroEulerComNtermos t) ++ " " ++ (tabelaValoresNumeroEuler (t-1))
+  | otherwise = "Erro"
+
+mediaQuantidadeN :: [Double] -> Double
+mediaQuantidadeN valores = (sum valores) / fromIntegral (length valores)
+
+desvioPadraoQuantidadeN :: [Double] -> Double
+desvioPadraoQuantidadeN valores = sqrt( 
+  (sum(
+    (map (\xi -> (xi - media)^2) valores)
+    ))
+   / fromIntegral (length valores) )
+  where
+    media = (mediaQuantidadeN valores)
+
+
+fatorialParaTaylor :: Integer -> Integer
+fatorialParaTaylor 0 = 1
+fatorialParaTaylor n = n * (fatorialParaTaylor (n-1))
+
+taylorSin :: Double -> Integer -> Double
+taylorSin x 0 = x
+taylorSin x n = ((((-1)^n) * valor) / fromIntegral fatorial ) + (taylorSin x (n-1))
+  where
+    fatorial = fatorialParaTaylor (2 * n + 1)
+    valor = x^(2 * n + 1)
+
+taylorCos :: Double -> Integer -> Double
+taylorCos x 0 = 1
+taylorCos x n = ((((-1)^n) * valor) / fromIntegral fatorial ) + (taylorCos x (n-1))
+  where
+    fatorial = fatorialParaTaylor (2 * n)
+    valor = x^(2 * n)
